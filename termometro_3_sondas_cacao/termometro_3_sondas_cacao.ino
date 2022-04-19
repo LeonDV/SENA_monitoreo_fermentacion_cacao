4b35f6ceef7b5acc98e2a07db01e000cd8b029f9
Leon-Valderrama <leonvalderrama@gmail.com>
2022-04-19 14:51:32 -0500

    Commit inicial

---
+++ termometro_3_sondas_cacao/termometro_3_sondas_cacao.ino
@@ -0,0 +1,16 @@
+/*
+ Name:		termometro_3_sondas_cacao.ino
+ Created:	19/04/2022 14:40:56
+ Author:	User
+*/
+
+// the setup function runs once when you press reset or power the board
+#include "variables.h"
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
